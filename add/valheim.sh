7e0692d1f5ff3fc007bff902cb56aad82fe5426e
Arokan13 <56687479+Arokan13@users.noreply.github.com>
2024-04-28 19:30:48 +0200

    Update valheim.sh

--- add/valheim.sh
+++ add/valheim.sh
@@ -4,18 +4,42 @@ cp /etc/resolv.conf /
 sed -i s/"nameserver ".*/"nameserver 1.1.1.1"/g /resolv.conf
 cp /resolv.conf /etc/resolv.conf
 
+#Manage Box86/64 version
+if [ "$BOX86" != "default" ];
+then	installed="$(apt-cache policy box86-rpi4arm64 | grep Installed | awk '{print $2}')"
+		wanted="$(apt-cache policy box86-rpi4arm64 | grep 500 | grep "$BOX86" | awk 'NR==1 {print $2}')"
+		latest="$(apt-cache policy box86-rpi4arm64 | grep Candidate | awk '{print $2}')"
+
+	if [ "$BOX86" = "latest" ] && [ "$installed" != "$latest" ];
+			then apt-get purge box86-rpi4arm64 -y && apt-get install box86-rpi4arm64=$latest;
+	elif [ "$BOX86" = "wanted" ] && [ "$installed" != "$wanted" ];
+			then apt-get purge box86-rpi4arm64 -y && apt-get install box86-rpi4arm64=$wanted;
+	fi;
+fi;
+
+if [ "$BOX64" != "default" ];
+then	installed="$(apt-cache policy box64-rpi4arm64 | grep Installed | awk '{print $2}')"
+		wanted="$(apt-cache policy box64-rpi4arm64 | grep 500 | grep "$BOX64" | awk 'NR==1 {print $2}')"
+		latest="$(apt-cache policy box64-rpi4arm64 | grep Candidate | awk '{print $2}')"
+
+	if [ "$BOX64" = "latest" ] && [ "$installed" != "$latest" ];
+			then apt-get purge box64-rpi4arm64 -y && apt-get install box64-rpi4arm64=$latest;
+	elif [ "$BOX64" = "wanted" ] && [ "$installed" != "$wanted" ];
+			then apt-get purge box64-rpi4arm64 -y && apt-get install box64-rpi4arm64=$wanted;
+	fi;
+fi;
+
 # Upgrade Valheim to latest Version
-if [ ! -f /valheim/start_server.sh ] || [ $UPDATE = true ]; then
+if [ ! -f /valheim/start_server.sh ] || [ $UPDATE = enabled ] || [ $UPDATE = 1 ]; then
 cd /steamcmd
 echo "Updating the server..."
 export LD_LIBRARY_PATH="/steamcmd/linux32:$LD_LIBRARY_PATH" && \
-box86 ./linux32/steamcmd +login anonymous +quit >/dev/null
+box86 ./linux32/steamcmd +login anonymous +quit >/dev/null;
 export LD_LIBRARY_PATH="/steamcmd/linux32:$LD_LIBRARY_PATH" && \
-box86 ./linux32/steamcmd +login anonymous +quit >/dev/null
+box86 ./linux32/steamcmd +login anonymous +quit >/dev/null;
 export LD_LIBRARY_PATH="/steamcmd/linux32:$LD_LIBRARY_PATH" && \
-box86 ./linux32/steamcmd +@sSteamCmdForcePlatformType linux +force_install_dir /valheim +login anonymous +app_update 896660 validate +quit
-fi
-#This is some of those "I don't know why it works that way, but it does", i.e. it doesn't work if I only put it once; the third try does the trick
+box86 ./linux32/steamcmd +@sSteamCmdForcePlatformType linux +force_install_dir /valheim +login anonymous +app_update 896660 validate +quit;
+fi;
 
 # Manage Persistency
 cp /scripts/start_server.sh.tpl	/valheim/start_server.sh
@@ -23,7 +47,7 @@ cp /scripts/start_server.sh.tpl	/valheim/start_server.sh
 cd /valheim
 
 #Pause Option
-if [ $PAUSE = true ]; then
+if [ $PAUSE = enabled ] || [ $PAUSE = 1 ]; then
 mkdir -p /data/logs
 cp /scripts/knockd.conf /etc/knockd.conf
 sed -i s.START_KNOCKD=0.START_KNOCKD=1.g /etc/default/knockd
@@ -38,3 +62,4 @@ export LOG_FILE="/data/logs/valheim-$(date '+%d.%m.%y - %H:%M:%S').log"
 touch $LOG_FILE
 echo "$LOG_FILE" > /data/logs/log-link.txt
 ./start_server.sh 2>/dev/null | tee -a "$LOG_FILE"
+sleep 600
